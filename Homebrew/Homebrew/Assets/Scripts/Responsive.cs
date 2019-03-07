e443778c148cd6e07d573222fae4c9d332a9eeca
Dragonite <mpeng@optimum.net>
2019-03-06 23:22:08 -0500

    got rid of the Interaction class, since everything it did could also be done by the Responsive superclass

--- Homebrew/Homebrew/Assets/Scripts/Responsive.cs
+++ Homebrew/Homebrew/Assets/Scripts/Responsive.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 
 public class Responsive : MonoBehaviour {
+    public int health = 1;
+
     protected virtual void Awake() {
     }
 
@@ -9,9 +11,37 @@ public class Responsive : MonoBehaviour {
         Collider2D collider = GetComponentInChildren<Collider2D>();
         foreach (PhysicalBottle bottle in PhysicalBottle.allBottles) {
             if (collider.bounds.Contains(bottle.transform.position)) {
-                GetComponent<Interactible>().Interact(bottle.Flags);
+                Interact(bottle.Flags);
                 Destroy(bottle.gameObject);
             }
+        }
+    }
+
+    public virtual void Kill(GameObject whoDidIt) {
+    }
+
+    public virtual void Interact(int potionFlags) {
+        int myFlags = GetComponent<HomebrewFlags>().Flags;
+
+        /*Whoever messes with this next, look down here
+        You probably want to deduct health when hit by valid elements,
+        but you can do other things too, like spawn a cloud of gas
+        around a detector or something
+        
+        and also comment out this message, i just did this to get your attention*/
+
+        if (PersistentInteraction.Recognized(potionFlags, Elements.FIRE)) {
+            Debug.Log(name + " was hit by an instance of Fire");
+        } else if (PersistentInteraction.Recognized(potionFlags, Elements.WATER)) {
+            Debug.Log(name + " was hit by an instance of Water");
+        } else if (PersistentInteraction.Recognized(potionFlags, Elements.WIND)) {
+            Debug.Log(name + " was hit by an instance of Wind");
+        } else if (PersistentInteraction.Recognized(potionFlags, Elements.THUNDER)) {
+            Debug.Log(name + " was hit by an instance of Thunder");
+        } else if (PersistentInteraction.Recognized(potionFlags, Elements.EARTH)) {
+            Debug.Log(name + " was hit by an instance of Earth");
+        } else {
+            // colliding elements not recognized
         }
     }
 }
