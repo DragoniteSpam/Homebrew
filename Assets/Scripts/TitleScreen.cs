0b154f5526c1b0ea15e95affd4992a5a59e2b03d
Peng <G00267682@stu.raritanval.edu>
2019-04-04 11:15:31 -0400

    title screen

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour {
+    public void BeginGame() {
+        SceneManager.LoadScene("SampleScene" /* too late to rename this */);
+    }
+}
