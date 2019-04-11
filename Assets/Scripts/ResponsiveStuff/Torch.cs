f29675825fe10b99a502d02776023bd214b56c28
Peng <G00267682@stu.raritanval.edu>
2019-04-11 13:22:39 -0400

    torches

---
+++ Assets/Scripts/ResponsiveStuff/Torch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HomebrewFlags))]
+public class Torch : Responsive {
+    public override void Interact(int potionFlags) {
+        SpriteRenderer sprite = transform.Find("Light").GetComponent<SpriteRenderer>();
+        if (!sprite.enabled) {
+            sprite.enabled = true;
+            OnLight();
+        }
+    }
+
+    public virtual void OnLight() {
+
+    }
+}
