e2e559f2176f303c0974705fcb6bc83600ce5877
Peng <G00267682@stu.raritanval.edu>
2019-04-11 13:31:01 -0400

    chris's sprites, hit zones

--- Assets/Scripts/ResponsiveStuff/Torch.cs
+++ Assets/Scripts/ResponsiveStuff/Torch.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(HomebrewFlags))]
 public class Torch : Responsive {
---
+++ Assets/Scripts/ResponsiveStuff/Zone.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HomebrewFlags))]
+public class Zone : Responsive {
+    // i attached a slime sprite renderer to the zone but it's just there so
+    // that you can see where-ish they are
+    public override void Interact(int potionFlags) {
+        HomebrewGame.CreateFloatingText(transform.position, "Hit zone", Color.blue);
+    }
+}
