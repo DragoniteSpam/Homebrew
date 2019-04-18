d8acfc1be149edc4ae88694084a480fb443b3adb
Peng <G00267682@stu.raritanval.edu>
2019-04-18 12:10:41 -0400

    helpful signposts

---
+++ Assets/Scripts/Signpost.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Signpost : MonoBehaviour {
+    public GameObject help;
+    
+    private void Update() {
+        Collider2D collider = GetComponentInChildren<Collider2D>();
+        Collider2D playerCollider = Player.Me.GetComponentInChildren<Collider2D>();
+        
+        // because screw the collision system
+        help.SetActive(playerCollider != null && collider.bounds.Intersects(playerCollider.bounds));
+    }
+}
