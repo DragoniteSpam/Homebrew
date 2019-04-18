56ad9640b6e5f784a1e8581d16f76f11d384a0aa
Peng <G00267682@stu.raritanval.edu>
2019-04-18 09:49:08 -0400

    the hazard prefabs broke again

--- Assets/Scripts/Hazards/HazardMagma.cs
+++ Assets/Scripts/Hazards/HazardMagma.cs
@@ -8,8 +8,10 @@ public class HazardMagma : Hazard {
         for (int i = 0; i < n; i++) {
             GameObject blob = Instantiate(HomebrewGame.Me.prefabHazard, new Vector3(what.transform.position.x + Random.Range(-r, r),
                 what.transform.position.y + Random.Range(-r, r), 0f/*what.transform.position.z*/), Quaternion.identity);
-            blob.GetComponentInChildren<SpriteRenderer>().sprite = HomebrewGame.Me.spritesMud[Random.Range(0, HomebrewGame.Me.spritesMud.Count - 1)];
-            blob.GetComponentInChildren<SpriteRenderer>().material.color = Color.red;
+
+            SpriteRenderer renderer = blob.GetComponentInChildren<SpriteRenderer>();
+            renderer.sprite = HomebrewGame.Me.spritesMud[Random.Range(0, HomebrewGame.Me.spritesMud.Count - 1)];
+            renderer.material.color = Color.red;
 
             blob.AddComponent<HazardMagma>();
         }
