bd43abda73803fcd64d6005ceaa1196d3873f940
Dragonite <mpeng@optimum.net>
2019-04-18 22:04:26 -0400

    typewriter text on signs

--- Assets/Scripts/Signpost.cs
+++ Assets/Scripts/Signpost.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 
 public class Signpost : MonoBehaviour {
+    private const float CHAR_PER_SECOND = 48;
+
+    private float t = 0f;
+    private string message;
+
+    GameObject helpRoot;
+    TextMesh threedtext;
+
+    void Awake() {
+        // ლ(ಠ益ಠლ)
+        helpRoot = transform.Find("Help").gameObject;
+        threedtext = helpRoot.transform.Find("Text").GetComponent<TextMesh>();
+        message = threedtext.text;
+    }
+
     private void Update() {
+        // because screw the collision system
         Collider2D collider = GetComponentInChildren<Collider2D>();
         Collider2D playerCollider = Player.Me.GetComponentInChildren<Collider2D>();
         
-        // because screw the collision system
-        transform.Find("Help").gameObject.SetActive(playerCollider != null && collider.bounds.Intersects(playerCollider.bounds));
+        if (playerCollider != null && collider.bounds.Intersects(playerCollider.bounds)) {
+            t = t + Time.deltaTime;
+            helpRoot.SetActive(true);
+            threedtext.text = message.Substring(0, (int)Mathf.Min(t * CHAR_PER_SECOND, message.Length));
+        } else {
+            t = 0f;
+            helpRoot.SetActive(false);
+        }
     }
 }
