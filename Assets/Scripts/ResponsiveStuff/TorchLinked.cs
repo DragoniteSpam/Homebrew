e02ca458478331259a4412e228a42dc36601c46b
Dragonite <mpeng@optimum.net>
2019-04-25 02:29:53 -0400

    linked torches

---
+++ Assets/Scripts/ResponsiveStuff/TorchLinked.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(HomebrewFlags))]
+public class TorchLinked : Torch {
+    public Torch[] linkedObjects;
+
+    public override void Interact(int potionFlags) {
+        base.Interact(potionFlags);
+    }
+
+    public override void OnLight() {
+        // check all linked objects before reacting
+        foreach (Torch torch in linkedObjects) {
+            if (!torch.transform.Find("Light").GetComponent<SpriteRenderer>().enabled) {
+                return;
+            }
+        }
+        foreach (Reaction r in reactionaryObjects) {
+            r.React(gameObject);
+        }
+    }
+}
