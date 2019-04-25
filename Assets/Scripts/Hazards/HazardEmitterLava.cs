a655ad70d422a90bda76389b80227d7004cf5394
Dragonite <mpeng@optimum.net>
2019-04-25 02:05:06 -0400

    lava emitters

---
+++ Assets/Scripts/Hazards/HazardEmitterLava.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HazardEmitterLava : MonoBehaviour {
+    public float period = 2f;
+
+    private float t;
+	void Awake() {
+        Blob();
+    }
+
+	void Update () {
+        t = t - Time.deltaTime;
+
+        if (t <= 0) {
+            Blob();
+        }
+	}
+
+    private void Blob() {
+        HazardMagma.SpawnBlob(gameObject);
+        t = period;
+    }
+}
